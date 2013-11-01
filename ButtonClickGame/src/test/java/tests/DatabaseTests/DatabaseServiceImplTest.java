6619945631b71e86e79b1980d76e1a227b25db9a
Makarchuk <Timur_makarchuk@mail.ru>
2013-11-01 22:28:54 +0400

    just commit

--- ButtonClickGame/src/test/java/tests/DatabaseTests/DatabaseServiceImplTest.java
+++ ButtonClickGame/src/test/java/tests/DatabaseTests/DatabaseServiceImplTest.java
@@ -39,7 +39,8 @@ public class DatabaseServiceImplTest {
     public void getCountTest(){
         DatabaseServiceImpl dbService = new DatabaseServiceImpl(context);
         int x = dbService.count;
-        Assert.assertNotNull(dbService.getAddress());
+        DatabaseServiceImpl dbService2 = new DatabaseServiceImpl(context);
+        Assert.assertEquals(x+1, dbService.count);
 
     }
 }
